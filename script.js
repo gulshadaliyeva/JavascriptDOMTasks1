9470297c470eaddf3da2e67929e96b15b7fc71d9
gulshadaliyeva <gulshadaliyeva02@gmail.com>
2023-03-02 01:44:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+// let h1 = document.querySelector(".h1s")
+// let div = document.querySelector(".divs")
+// let nextSibling = h1.nextElementSibling;
+// let prevSiBling = h1.previousElementSibling;
+// let nextSiBling = h1.nextSibling;
+// let prevsipling = h1.previousSibling;
+// let parentElement = h1.parentElement;
+// let parenode = h1.parentNode;
+// let chilnode =div.childNodes;
+// let children= div.children;
+// console.log(chilnode);
+
+
+
+
+// let h1s = document.querySelector(".h1s")
+// console.log(h1s.getAttribute("class"));
+// let btn = document.querySelector("button")
+// console.log(btn.hasAttribute('disabled'));
+// console.log(btn.setAttribute("class","click-btn"));
+// btn.setAttribute("class", "item");
+// btn.removeAttribute('class')
+
+
+
+// let div = document.querySelector("div")
+// div.className = "foo"
+// console.log(div.className);
+// console.log(div.classList);
+// for (let cssClass of text) {
+//     console.log(cssClass);
+// }
+// for (let cssClass of div.classList) {
+//     console.log(cssClass);
+// }
+// div.classList.add("item","info")
+// div.classList.remove("item")
+// div.classList.contains("item")
+// console.log(div.classList.contains("item"));
+
+
+// let div = document.querySelector("div")
+// console.log(div.offsetHeight);
+// console.log(div.offsetWidth);
+// console.log(div.offsetLeft);
+// console.log(div.offsetParent);
+// console.log(div.offsetTop);
+// console.log(div.clientHeight);
+// console.log(div.clientWidth);
+// console.log(div.clientLeft);
+// console.log(div.clientTop);
+// console.log(div.scrollHeight);
+// console.log(div.scrollLeft);
+// console.log(div.scrollTop);
+// console.log(div.scrollWidth);
+
+// let btn = document.querySelector(".push")
+// function func() {
+//     alert("salam"+ btn)
+// }
+// let btn = document.querySelector(".push")
+// btn.onclick = function() {
+//     alert("cox sagol")
+// }
+// let div = document.querySelector(".divs")
+// let p = document.querySelector("p.error")
+// btn.addEventListener('click', function(){
+//     p.innerText = "Error var"
+//     setTimeout(() => {
+//         p.innerText = ""
+//     }, 5000);
+// })
